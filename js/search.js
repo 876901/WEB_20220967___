8185be6564b368c960127a3a9512d8142b63ad06
김경진 <z876901@naver.com>
2023-06-12 15:46:12 +0000

    7주차

--- js/search.js
+++ js/search.js
@@ -14,21 +14,19 @@
 // }
 
 document.getElementById("search_btn").addEventListener('click', search_message);
-  // 검색 제한 단어 선언 및 초기화
-var search_array=[];
-  const filltering = ["미쳐", "죽을래", "뒤져라"];
-function search_message() {
-  if (filltering.includes(search_str.value)) {
-    alert("검색어에 제한된 단어가 포함되어 있습니다!");
 
-  }else{
-	  alert("검색을 수행합니다");
-	  search_array.push(search_str.value);
-  }
-  
-	  let text = document.getElementById("search_message").innerHTML = search_array.toString(); // 값 변환
-  document.querySelector("#form_main").submit();
-  // 검색어 출력 및 로그
-  search_message.innerHTML = search_str.value;
-  console.log(search_str.value);
-}
+var search_array = [];
+const filltering = ["시발","미친","개새끼"];
+
+function search_message(){
+   let search_str = document.getElementById("search_txt").value;
+   if(filltering.includes(search_str)){
+      alert("검색 제한");
+   }
+   else{
+      alert("검색 수행");
+      document.querySelector("#form_main").submit();
+      search_array.push(search_str.value);      
+   }
+   let text = document.getElementById("search_message").innerHtml = search_array.toString();
+   search_message.innerHTML = search_str.value;
